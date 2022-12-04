2385d4f1c43154d12c96f7cb8f60eb87d13e2798
Giles Dring <giles@dringtech.com>
2022-12-04 18:04:15 +0000

    Chasing delia

--- _lib/sketch/card.ts
+++ _lib/sketch/card.ts
@@ -1,8 +1,10 @@
 import { Drawable } from 'lib/drawable.ts';
 import { Figure } from 'lib/figure.ts';
+import { P5I } from '../../../../deno/.cache/deno/npm/registry.npmjs.org/p5i/0.4.2/dist/p5i.d.ts';
 import { Delia } from '../parts/delia.ts';
 
 const figures: Drawable[] = [];
+let delia: Delia;
 
 export function setup(context: P5I) {
   const { windowWidth, windowHeight, createCanvas, stroke, frameRate, random } = context;
@@ -11,7 +13,7 @@ export function setup(context: P5I) {
   createCanvas(WIDTH, HEIGHT);
   stroke(255);
   frameRate(30);
-  const delia = new Delia(WIDTH / 2, HEIGHT / 2);
+  delia = new Delia(WIDTH / 2, HEIGHT / 2);
   figures.push(delia);
   figures.push(new Figure({ name: 'Giles', x: WIDTH * 1 / 5, y: random(0.5, 1.5) * HEIGHT / 4, target: delia }, context));
   figures.push(new Figure({ name: 'Rebecca', x: WIDTH * 2 / 5, y: random(0.5, 1.5) * HEIGHT / 4, target: delia }, context));
@@ -21,10 +23,10 @@ export function setup(context: P5I) {
 
 export function draw(context: P5I) {
   const { height, push, pop, scale, translate, background, clear } = context;
+  clear();
   push();
   scale(1, -1);
   translate(0, -height);
-  clear();
 
   figures.forEach(f => f.draw(context));
   pop();
@@ -32,4 +34,8 @@ export function draw(context: P5I) {
 
 export function windowResized({ windowWidth, windowHeight, resizeCanvas }: P5I) {
   resizeCanvas(windowWidth, windowHeight);
+}
+export function mouseMoved({ mouseX, mouseY }: P5I) {
+  delia.x = mouseX;
+  delia.y = mouseY;
 }
--- assets/js/xmas.ts
+++ assets/js/xmas.ts
@@ -1,8 +1,8 @@
 import { p5i } from 'p5i';
-import { setup, draw, windowResized } from 'lib/sketch/card.ts';
+import { setup, draw, windowResized, mouseMoved } from 'lib/sketch/card.ts';
 
 const { mount } = p5i();
 
 addEventListener('DOMContentLoaded', () => {
-  mount(document.getElementById('cardCanvas'), { setup, draw, windowResized });
+  mount(document.getElementById('cardCanvas'), { setup, draw, windowResized, mouseMoved });
 });
